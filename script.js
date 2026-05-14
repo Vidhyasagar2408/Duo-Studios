8fd32fb28de8cdf9db91ca5ae393ae9f18ab68c3
vidhyasagar <vs1522250@gmail.com>
2026-05-14 16:22:43 +0530

    duo studios

--- script.js
+++ script.js
@@ -35,76 +35,157 @@ function init() {
 
 init();
 
-let cursor = document.querySelector(".cursor");
-let main = document.querySelector(".main");
-let video = document.querySelector(".page1 video");
-main.addEventListener("mousemove", function (dets) {
-  gsap.to(cursor, {
-    left: dets.x + "px",
-    top: dets.y + "px",
-    duration: 0.2,
+function page1Animation() {
+  let cursor = document.querySelector(".cursor");
+  let main = document.querySelector(".main");
+  let video = document.querySelector(".page1 video");
+  main.addEventListener("mousemove", function (dets) {
+    gsap.to(cursor, {
+      left: dets.x + "px",
+      top: dets.y + "px",
+      duration: 0.6,
+    });
   });
-});
 
-video.addEventListener("mouseenter", function () {
-  cursor.classList.add("active");
-  cursor.innerHTML = "SOUND ON";
-});
+  video.addEventListener("mouseenter", function () {
+    cursor.classList.add("active");
+    cursor.innerHTML = "SOUND ON";
+  });
 
-video.addEventListener("mouseleave", function () {
-  cursor.classList.remove("active");
-  cursor.innerHTML = "";
-});
+  video.addEventListener("mouseleave", function () {
+    cursor.classList.remove("active");
+    cursor.innerHTML = "";
+  });
 
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".page1 h1",
-    scroller: ".main",
-    markers: true,
-    start: "top 28%",
-    end: "top 0",
-    scrub: 2,
-  },
-});
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".page1 h1",
+      scroller: ".main",
+      start: "top 28%",
+      end: "top 0",
+      scrub: 2,
+    },
+  });
 
-tl.to(
-  ".page1 h1",
-  {
-    x: -120,
+  tl.to(
+    ".page1 h1",
+    {
+      x: -120,
+      duration: 1,
+    },
+    "anim",
+  );
+
+  tl.to(
+    ".page1 h2",
+    {
+      x: 100,
+      duration: 1,
+    },
+    "anim",
+  );
+
+  tl.to(
+    ".page1 video",
+    {
+      width: "90%",
+    },
+    "anim",
+  );
+
+  let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".page1 h1",
+      scroller: ".main",
+      markers: true,
+      start: "top -60%",
+      end: "top -100%",
+      scrub: 2,
+    },
+  });
+
+  tl2.to(".main", {
+    backgroundColor: "white",
+    color: "black",
+  });
+}
+
+page1Animation();
+
+function page2Animation() {
+  let h1 = document.querySelector(".page2 h1");
+  let h2 = document.querySelector(".page2 h2");
+  let p = document.querySelector(".page2 p");
+
+  let tl3 = gsap.timeline();
+
+  tl3.from(
+    h1,
+    {
+      y: -80,
+      opacity: 0,
+      scrollTrigger: {
+        scroller: ".main",
+        trigger: h1,
+        start: "top 40%",
+        end: "top 80%",
+        scrub: 3,
+      },
+    },
+    "page2",
+  );
+
+  tl3.from(
+    h2,
+    {
+      x: -80,
+      opacity: 0,
+      scrollTrigger: {
+        scroller: ".main",
+        trigger: h2,
+        start: "top 40%",
+        end: "top 80%",
+        scrub: 3,
+      },
+    },
+    "page2",
+  );
+
+  tl3.from(
+    p,
+    {
+      x: 160,
+      opacity: 0,
+      scrollTrigger: {
+        scroller: ".main",
+        trigger: p,
+        start: "top 40%",
+        end: "top 80%",
+        scrub: 3,
+      },
+    },
+    "page2",
+  );
+}
+
+page2Animation();
+
+function page3Animation() {
+  _;
+  let page3 = document.querySelector(".page3-part2");
+
+  gsap.from(page3, {
+    y: -100,
+    opacity: 0,
     duration: 1,
-  },
-  "anim",
-);
+    stagger: 1,
+    scrollTrigger: {
+      scroller: ".main",
+      trigger: page3,
+      start: "top 40%",
+      end: "top 80%",
+    },
+  });
+}
 
-tl.to(
-  ".page1 h2",
-  {
-    x: 100,
-    duration: 1,
-  },
-  "anim",
-);
-
-tl.to(
-  ".page1 video",
-  {
-    width: "90%",
-  },
-  "anim",
-);
-
-let tl2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".page1 h1",
-    scroller: ".main",
-    markers: true,
-    start: "top -60%",
-    end: "top -100%",
-    scrub: 2,
-  },
-});
-
-tl2.to(".main", {
-  backgroundColor: "white",
-  color: "black",
-});
+page3Animation();
